dcef1814afb1bdbcc030c9f4c2a3c5db942eb642
Akram Lagrazna <lagrazna@outlook.it>
2021-01-25 10:12:15 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var apiRouter = require('./routes/api');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+/*
+app.use(function(request, response) {
+  console.log("In comes a request to: " + request.url);
+});*/
+
+app.use('/api', apiRouter);
+app.use('/users', usersRouter);
+
+
+
+
+module.exports = app;
---
+++ routes/api.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://admin:admin11@cw2.it8bj.mongodb.net/cw2?retryWrites=true&w=majority';
+var db;
+MongoClient.connect(url, (err, client) => {
+  db = client.db('cw2');
+  })
+
+app.param('collectionName', (req, res, next, collectionName) => {
+  req.collection = db.collection(collectionName);
+  return next()
+})
+
+/* GET home page. */
+app.get('/', (req, res, next) => {  
+  res.send('Select a collection, e.g., /collection/messages')
+})
+/* Collection pages */
+// VIEW COLLECTIONS
+app.get('/collection/:collectionName', (req, res, next) => {
+  req.collection.find({}).toArray((e, results) => {
+    if (e) return next(e)      
+    res.send(results)    
+  }
+  )})
+  
+const ObjectID = require('mongodb').ObjectID;
+app.get('/collection/:collectionName/:id', (req, res, next) => {    
+  req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+    if (e) return next(e)        
+    res.send(result)    
+  })
+})
+// ADD NEW
+app.post('/collection/:collectionName', (req, res, next) => {  
+  req.collection.insert(req.body, (e, results) => {
+    if (e) return next(e)    
+    res.send(results.ops)  
+  })
+})
+// UPDATE 
+app.put('/collection/:collectionName/:id', (req, res, next) => {  
+  req.collection.update(    
+    {_id: new ObjectID(req.params.id)},    
+    {$set: req.body},    
+    {safe: true, multi: false}, 
+    (e, result) => {
+      if (e) return next(e)      
+      res.send((result.result.n === 1) ? {msg: 'updated'} : {msg: 'error'})    
+    })
+  })
+// DELETE
+app.delete('/collection/:collectionName/:id', (req, res, next) => {  
+  req.collection.deleteOne(    
+    {_id: ObjectID(req.params.id)}, 
+    (e, result) => {
+      if (e) return next(e)        
+      res.send((result.result.n === 1) ? {msg: 'deleted'} : {msg: 'error'})  
+    })
+  })
+
+module.exports = app;
