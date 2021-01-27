60851645be20fd05c7284d068c3ae8c800a639b0
Akram Lagrazna <lagrazna@outlook.it>
2021-01-27 11:36:04 -0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,11 @@ app.param('collectionName', (req, res, next, collectionName) => {
   req.collection = db.collection(collectionName);
   return next()
 })
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();  
+});
 /* GET home page. */
 app.get('/', (req, res, next) => {  
   res.send('Select a collection, e.g., /collection/messages')
