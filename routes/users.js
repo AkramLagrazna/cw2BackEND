48a4d824ff63ab5d840c20f75c019b4d4055a82c
Akram Lagrazna <lagrazna@outlook.it>
2021-01-26 16:54:39 -0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
-  app.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.send('respond with a resource');
 });
 
